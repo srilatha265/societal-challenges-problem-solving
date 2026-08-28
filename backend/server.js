334c877cb0175080c5c23cb49ab7a0b805dec5aa
srilatha265 <srilathasankurathri498@gmail.com>
2026-08-28 19:55:36 +0530

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs').promises;
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
